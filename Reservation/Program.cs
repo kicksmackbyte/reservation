4db64b23790d4ea4e065a14ea62b9fe5c094e456
Alberto Garcia <alberto@agarcia.io>
2024-07-31 17:33:22 -0700

    Barebones graphQL app

--- Reservation/Program.cs
+++ Reservation/Program.cs
@@ -1,6 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+public class Book
+{
+    public string Title { get; set; }
+    public Author Author { get; set; }
+}
 
-app.MapGet("/", () => "Hello World!");
+public class Author
+{
+    public string Name { get; set; }
+}
 
-app.Run();
+public class Query
+{
+    public Book GetBook() =>
+        new Book
+        {
+            Title = "C# in depth.",
+            Author = new Author
+            {
+                Name = "Jon Skeet"
+            }
+        };
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services
+            .AddGraphQLServer()
+            .AddQueryType<Query>();
+
+        var app = builder.Build();
+
+        app.MapGraphQL();
+        app.Run();
+    }
+}
